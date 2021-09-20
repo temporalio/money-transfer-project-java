ac5e1b2a04e14dcc4127efd1d9899b8ca0c2fa9b
Dmitry Spikhalskiy <dmitry@spikhalskiy.com>
2021-09-20 18:24:46 -0400

    Update dependencies, cleanup irrelevant template comments, remove .idea from the project (#6)

--- src/main/java/moneytransferapp/AccountActivity.java
+++ src/main/java/moneytransferapp/AccountActivity.java
@@ -3,12 +3,15 @@ package moneytransferapp;
 // @@@SNIPSTART money-transfer-project-template-java-activity-interface
 
 import io.temporal.activity.ActivityInterface;
+import io.temporal.activity.ActivityMethod;
 
 @ActivityInterface
 public interface AccountActivity {
 
+    @ActivityMethod
     void deposit(String accountId, String referenceId, double amount);
 
+    @ActivityMethod
     void withdraw(String accountId, String referenceId, double amount);
 }
 // @@@SNIPEND
