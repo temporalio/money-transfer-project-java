c9b66bad762f2116563fcf0e030ad9615f6ec882
rachfop <prachford@icloud.com>
2024-05-15 08:30:28 -0700

    Remove other snips

--- src/main/java/moneytransferapp/Shared.java
+++ src/main/java/moneytransferapp/Shared.java
@@ -1,8 +1,6 @@
 package moneytransferapp;
 
-// @@@SNIPSTART money-transfer-project-template-java-shared-constants
 public interface Shared {
 
     static final String MONEY_TRANSFER_TASK_QUEUE = "MONEY_TRANSFER_TASK_QUEUE";
 }
-// @@@SNIPEND
